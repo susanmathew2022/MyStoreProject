22cb29ece71c9a9796695ea15158156c0249af11
susanmathew2022 <susanmathew2022@gmail.com>
2021-06-15 14:31:23 -0500

    creating components with basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './Users/login/login.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { ProductAddComponent } from './products/product-add/product-add.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'home', component: HomeComponent},
+{path: 'login', component: LoginComponent},
+{path:'products', children:[
+ {path:'',component:ProductListComponent},
+ {path:':id',component:ProductAddComponent},
+ {path:':id/edit',component:ProductAddComponent}
+]
+},
+{path: '**', component:PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './Users/login/login.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { ProductAddComponent } from './products/product-add/product-add.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent,
+    ProductDetailsComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
